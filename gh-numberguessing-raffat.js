dd9d846a291aaec7fed5697442c9e7b01dd4ad96
RaffatAlam <164245329+RaffatAlam@users.noreply.github.com>
2024-04-12 14:36:35 +0500

    Add files via upload

---
+++ gh-numberguessing-raffat.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("Welcome to codewth Raffat - CLI Number Guessing Game");
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessNumber",
+        type: "number",
+        message: "Enter your guess number(Number Limit from 1 to 5):",
+    },
+]);
+if (answer.userGuessNumber === randomNumber) {
+    console.log("Congratulations! you guess a correct number. successfully done!");
+}
+else {
+    console.log("Sorry, you guess a wrong number. please try again. dont giveup!");
+}
